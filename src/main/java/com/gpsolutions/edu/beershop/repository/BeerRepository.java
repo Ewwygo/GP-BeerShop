ec06b164ffb1ded4e1beb17fe01b357b21fbdab5
Ewwygo <kudin9850@gmail.com>
2020-02-22 14:47:04 +0300

    Revert "Revert "Revert "add Docker, more service functional, refactor orderDTO & mapper"""
    This reverts commit 7a2be759d799f8f0ab4468fcf227bc0552ea2885.

--- src/main/java/com/gpsolutions/edu/beershop/repository/BeerRepository.java
+++ src/main/java/com/gpsolutions/edu/beershop/repository/BeerRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BeerRepository extends JpaRepository<BeerEntity, Long> {
-
-    Optional<BeerEntity> findByTitle(String title);
 
 }
--- src/test/java/com/gpsolutions/edu/beershop/controller/AbstractControllerTest.java
+++ src/test/java/com/gpsolutions/edu/beershop/controller/AbstractControllerTest.java
@@ -2,20 +2,26 @@ package com.gpsolutions.edu.beershop.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gpsolutions.edu.beershop.dto.UserSignInResponse;
+import com.gpsolutions.edu.beershop.entity.AuthInfoEntity;
 import com.gpsolutions.edu.beershop.entity.UserEntity;
-import com.gpsolutions.edu.beershop.repository.OrderRepository;
+import com.gpsolutions.edu.beershop.repository.AuthInfoRepository;
 import com.gpsolutions.edu.beershop.repository.UserRepository;
+import com.gpsolutions.edu.beershop.security.LoadClientDetailService;
 import com.gpsolutions.edu.beershop.security.Roles;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.management.relation.Role;
+import java.util.List;
 import java.util.Optional;
 
 import static com.gpsolutions.edu.beershop.security.Roles.ADMIN;
@@ -28,7 +34,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Log
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource("classpath:application-test.properties")
 @AutoConfigureMockMvc
 public abstract class AbstractControllerTest {
 
@@ -39,23 +44,20 @@ public abstract class AbstractControllerTest {
     @Autowired
     protected PasswordEncoder passwordEncoder;
 
-    private final String SALT = "my salt stonks";
-
+    @MockBean
+    protected AuthInfoRepository authInfoRepository;
     @MockBean
     protected UserRepository userRepository;
 
-    @MockBean
-    protected OrderRepository orderRepository;
-
     protected String signInAsClient() throws Exception {
 
-        final UserEntity user = createUserInfo(CLIENT);
-        willReturn(Optional.of(user)).given(userRepository).findByLogin("vasya@email.com");
+        final AuthInfoEntity authInfo = createAuthInfo(CLIENT);
+        willReturn(Optional.of(authInfo)).given(authInfoRepository).findByLogin("vasya@email.com");
 
         final String response = mockMvc.perform(post("/beer-shop-app/client/sign-in")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\n" +
-                        "  \"login\" : \"vasya@email.com\",\n" +
+                        "  \"email\" : \"vasya@email.com\",\n" +
                         "  \"password\" : \"qwerty\"\n" +
                         "}"))
                 // then
@@ -67,13 +69,13 @@ public abstract class AbstractControllerTest {
 
     protected String signInAsAdmin() throws Exception {
 
-        final UserEntity user = createUserInfo(ADMIN);
-        willReturn(Optional.of(user)).given(userRepository).findByLogin("admin@email.com");
+        final AuthInfoEntity authInfo = createAuthInfo(ADMIN);
+        willReturn(Optional.of(authInfo)).given(authInfoRepository).findByLogin("admin@email.com");
 
         final String response = mockMvc.perform(post("/beer-shop-app/client/sign-in")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\n" +
-                        "  \"login\" : \"admin@email.com\",\n" +
+                        "  \"email\" : \"admin@email.com\",\n" +
                         "  \"password\" : \"qwerty\"\n" +
                         "}"))
                 // then
@@ -83,16 +85,16 @@ public abstract class AbstractControllerTest {
         return "Bearer " + objectMapper.readValue(response, UserSignInResponse.class).getToken();
     }
 
-    protected UserEntity createUserInfo(Roles role) {
+    protected AuthInfoEntity createAuthInfo(Roles role) {
         final UserEntity user = new UserEntity();
         user.setRole(role);
-        user.setId(1l);
-        user.setFio("Пупкин Василий Иванович");
-        user.setPhoneNumber("+3752912345678");
-        user.setInfo("Молодой инженер");
-        user.setLogin("vasya@email.com");
-        user.setPassword(passwordEncoder.encode("qwerty"+SALT));
-        return user;
+        user.setEmail("vasya@email.com");
+
+        final AuthInfoEntity authInfo = new AuthInfoEntity();
+        authInfo.setLogin(user.getEmail());
+        authInfo.setPassword(passwordEncoder.encode("qwerty"));
+        authInfo.setUser(user);
+        return authInfo;
     }
 
 
